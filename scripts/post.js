60a2538fdcecdfb24932483f32ae0c8e15972e52
Bill <oupquv@gmail.com>
2024-06-21 18:07:04 +0800

    :feature: 新增 issues 內容作為文章

---
+++ scripts/post.js
@@ -0,0 +1,17 @@
+const axios = require('axios')
+const fs = require('fs')
+
+const init = async () => {
+  const response = await axios('https://api.github.com/repos/akakaki/akakaki.github.io/issues')
+  const target = response.data.reduce((acc, cur) => {
+    if (cur.author_association !== 'OWNER') acc
+    return acc.concat({
+      id: cur.id,
+      body: cur.body,
+    })
+  }, [])
+  fs.unlink('./source/_posts/template.md',function() {})
+  for (const item of target) fs.writeFile(`./source/_posts/${item.id}.md`, item.body, function () {})
+}
+
+init()
