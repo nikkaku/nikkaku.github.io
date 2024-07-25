a5e4f0860248991030cffa95f30ff04659f78a9a
Amuuuuuuuu <oupquv@gmail.com>
2024-07-26 01:35:34 +0800

    :fix: removc next config script

--- script/user.js
+++ script/user.js
@@ -8,10 +8,6 @@ const init = async () => {
   const configYmlFile = './_config.yml'
   const configYml = await fs.readFileSync(configYmlFile, 'utf-8')
   fs.writeFileSync(configYmlFile, configYml.replace(/(author: ).*/, `$1${name}`), function () {})
-  
-  const configNextYmlFile = './_config.next.yml'
-  const configNextYml = await fs.readFileSync(configNextYmlFile, 'utf-8')
-  fs.writeFileSync(configNextYmlFile, configNextYml.replace(/.*(avatar:.*\n.*)(\n.*)(url: )(.*)( #)/, `$1$2$3${avatar_url}$5`), function () {})
 }
 
 init()
